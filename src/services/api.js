d6cff5c577c285f8776faae5f1bba019295d5efb
Abhay <abhay.srdt@gmail.com>
2026-09-24 12:24:19 +0530

    Authenticaion and authrization done

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,19 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:"http://localhost:8080/api"
+    baseURL:"http://localhost:8080/api",
+    
 })
 
+
+api.interceptors.request.use((config)=>{
+    const savedUser=localStorage.getItem("user");
+    if(savedUser){
+        const token = JSON.parse(savedUser).token;
+        if(token){
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+    }
+    return config;
+})
 export default api;
